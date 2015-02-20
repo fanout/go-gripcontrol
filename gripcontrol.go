575f32b0c9e17ac7ead7f126cb5516b1a4b3a99c
Kon <bokarius@comcast.net>
2015-02-19 18:24:59 -0800

    added features to gripcontrol.go

--- gripcontrol.go
+++ gripcontrol.go
@@ -10,13 +10,57 @@ package gripcontrol
 import "fmt"
 import "unicode/utf8"
 import "encoding/json"
+import "strings"
+
+func CreateGripChannelHeader(channels []*Channel) string {
+    var parts []string
+    for _, channel := range channels {   
+        s := channel.Name
+        if (channel.PrevId != "") {
+            s += "; prev-id=" + channel.PrevId
+        }
+        parts = append(parts, s)
+    }
+    return strings.Join(parts, ", ")
+}
+
+func DecodeWebsocketEvents(body string) ([]*WebSocketEvent, error) {
+    out := make([]*WebSocketEvent, 0)
+    for start := 0; start < utf8.RuneCountInString(body); {
+        partialBody := body[start:]
+        if (partialBody == "\r\n") {
+            break
+        }
+        at := strings.Index(partialBody, "\r\n")
+        if (at == -1) {
+            return nil, &GripFormatError{err: "bad format"} 
+        }
+        start += at + 2
+        contentStart := at + 2
+        typeline := partialBody[0:at]
+        at = strings.Index(typeline, " ")
+        var event *WebSocketEvent
+        if (at != -1) {
+            etype := typeline[:at]
+            var clen int
+            fmt.Sscanf(typeline[at + 1:], "%x", &clen)
+            content := partialBody[contentStart:contentStart + clen]           
+            start += clen + 2
+            event = &WebSocketEvent{Type:etype, Content:content}
+        } else {
+            event = &WebSocketEvent{Type:typeline}
+        }
+        out = append(out, event)
+    }
+    return out, nil
+}
 
 func EncodeWebsocketEvents(events []*WebSocketEvent) string {
     out := ""
     for _, event := range events {
         if (event.Content != "") {
-            out += fmt.Sprintf("%s %02x\r\n%s\r\n", event.Type, 
-                    utf8.RuneCountInString(event.Content), event.Content)
+            out += fmt.Sprintf("%s %x\r\n%s\r\n", event.Type, 
+                    len(event.Content), event.Content)
         } else {
             out += fmt.Sprintf("%s\r\n", event.Type)
         }
@@ -35,4 +79,12 @@ func WebsocketControlMessage(messageType string,
     out["type"] = messageType
     message, err := json.Marshal(out)
     return string(message), err
+}
+
+type GripFormatError struct {
+    err string
+}
+
+func (e GripFormatError) Error() string {
+    return e.err
 }
